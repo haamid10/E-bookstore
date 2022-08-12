f3df7fd32415c2e5e229bbb6e2b429e5668fe50d
haamid10 <apdixamiid919@gmail.com>
2022-08-12 12:26:02 +0300

    home page2

--- home.php
+++ home.php
@@ -45,9 +45,33 @@ if(isset($_POST['add_to_cart'])){
    <link rel="stylesheet" href="css/style.css">
 
 </head>
-<body>
+<body >
    
 <?php include 'header.php'; ?>
+
+<div class="bg-gray-900">
+   .
+   .
+<div class="w-full mt-72 bg-gray-900">
+  <div class="p-0 flex items-center text-xl overflow-hidden min-h-screen-75">
+    <div class=" mx-auto px-4">
+      <div class="items-center text-left flex flex-wrap -mx-4">
+        <div class="px-4  text-white w-full lg:w-6/12">
+          <h1 class="text-7xl text font-bold leading-tight ">Read with us,to speed your development...</h1>
+          <p class="mt-4 mb-4 text-xl leading-relaxed text-blueGray-400">We aim high at being focused on building relationships with our clients and community. Using our creative books drives this foundation.</p>
+          <div class="flex flex-wrap -mx-4 mt-8">
+            <div class="px-4 pr-2 relative w-full sm:w-8/12">
+              <div class="mb-3 pt-0"></div>
+            </div>
+            <div class="px-4 pl-0 relative w-16 sm:w-8/12"><a href="login.php" class="inline-block outline-none focus:outline-none align-middle transition-all duration-150 ease-in-out w-44 uppercase border border-solid font-bold last:mr-0 mr-2  text-white bg-pink-500 border-pink-500 active:bg-pink-600 active:border-pink-600 text-xl px-6 py-4  shadow hover:shadow-lg rounded-md">Join us</a></div>
+          </div>
+        </div>
+        <div class="px-4 relative w-full lg:w-6/12"><img src="https://demos.creative-tim.com/notus-pro-react/static/media/example-17.76927438.svg" class="w-full lg:ml-12" alt="..."></div>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <section class="home">
 
@@ -59,7 +83,7 @@ if(isset($_POST['add_to_cart'])){
 
 </section>
 
-
+</div>
 <section class="relative w-full h-full">
 <div class="absolute hidden w-full bg-gray-50 lg:block h-96"></div>
  <div class="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
@@ -104,8 +128,11 @@ if(isset($_POST['add_to_cart'])){
       <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
       <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
       <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-      <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+      <input  type="submit"  value="add to cart" name="add_to_cart"
+            class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+          >
      </form>
+     
       </div>
       <?php
          }
@@ -118,104 +145,19 @@ if(isset($_POST['add_to_cart'])){
    </div>
 
    <div class="load-more" style="margin-top: 2rem; text-align:center">
+  
+         
       <a href="shop.php" class="option-btn">load more</a>
    </div>
 
 
+ 
    
  </div>
 </section>
 
 <!-- tailwind landing 1 -->
-<div class="relative w-full h-full">
-  <div class="absolute hidden w-full bg-gray-50 lg:block h-96"></div>
-  <div class="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-    <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-      <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-        <span class="relative inline-block">
-          <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-gray-400 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
-            <defs>
-              <pattern id="2c67e949-4a23-49f7-bf27-ca140852cf21" x="0" y="0" width=".135" height=".30">
-                <circle cx="1" cy="1" r=".7"></circle>
-              </pattern>
-            </defs>
-            <rect fill="url(#2c67e949-4a23-49f7-bf27-ca140852cf21)" width="52" height="24"></rect>
-          </svg>
-          <span class="relative">Affordable</span>
-        </span>
-        for everyone
-      </h2>
-      <p class="text-base text-gray-700 md:text-lg">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
-      </p>
-    </div>
-    <div class="grid max-w-screen-md gap-10 md:grid-cols-2 sm:mx-auto">
-      <div>
-        <div class="p-8 bg-gray-900 rounded">
-          <div class="mb-4 text-center">
-            <p class="text-xl font-medium tracking-wide text-white">
-              Starter Plan
-            </p>
-            <div class="flex items-center justify-center">
-              <p class="mr-2 text-5xl font-semibold text-white lg:text-6xl">
-                $39
-              </p>
-              <p class="text-lg text-gray-500">/ month</p>
-            </div>
-          </div>
-          <ul class="mb-8 space-y-2">
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-accent-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">10 deploys per day</p>
-            </li>
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-accent-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">10 GB of storage</p>
-            </li>
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-accent-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">3 domains</p>
-            </li>
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-accent-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">SSL Certificates</p>
-            </li>
-          </ul>
-          <button
-            type="submit"
-            class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-          >
-            Get Now
-          </button>
-        </div>
-        <div class="w-11/12 h-2 mx-auto bg-gray-900 rounded-b opacity-75"></div>
-        <div class="w-10/12 h-2 mx-auto bg-gray-900 rounded-b opacity-50"></div>
-        <div class="w-9/12 h-2 mx-auto bg-gray-900 rounded-b opacity-25"></div>
-      </div>
-      
-    </div>
-  </div>
-</div>
+
 <!-- end -->
 
 
