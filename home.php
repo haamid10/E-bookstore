810fb7775ac365b5e6575393872ea918abd627e0
haamid10 <apdixamiid919@gmail.com>
2022-08-14 22:18:56 +0300

    add1

--- home.php
+++ home.php
@@ -58,35 +58,6 @@ if(isset($_POST['add_to_cart'])){
 
 
 
-<!-- content -->
-
-
-
-<!-- productss -->
-<!-- 
-<section class="relative w-full h-full">
-
-   <div class=" w-full bg-gray-50 lg:block h-96"">
-
-      <?php
-         $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
-         if(mysqli_num_rows($select_products) > 0){
-            while($fetch_products = mysqli_fetch_assoc($select_products)){
-      ?>
-      <div class="box">
-         <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
-         <div class="name"><?php echo $fetch_products['name']; ?></div>
-         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-         <a href="admin_products.php?update=<?php echo $fetch_products['id']; ?>" class="option-btn">update</a>
-         <a href="admin_products.php?delete=<?php echo $fetch_products['id']; ?>" class="delete-btn" onclick="return confirm('delete this product?');">delete</a>
-      </div>
-      <?php
-         }
-      }else{
-         echo '<p class="empty">no products added yet!</p>';
-      }
-      ?>
-   </div>
 
 </section> -->
 
@@ -95,145 +66,45 @@ if(isset($_POST['add_to_cart'])){
 
 
 </div>
-<section class="relative w-full h-full flex flex-row">
-<div class="absolute hidden w-full bg-gray-50 lg:block h-96"></div>
- <div class="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-    <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
 
-    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 rounded-md shadow-sm p-4  shadow-gray-600 sm:text-4xl md:mx-auto">
-        <span class="relative inline-block">
-          <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-gray-400 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
-            <defs>
-              <pattern id="2c67e949-4a23-49f7-bf27-ca140852cf21" x="0" y="0" width=".135" height=".30">
-                <circle cx="1" cy="1" r=".7"></circle>
-              </pattern>
-            </defs>
-            <rect fill="url(#2c67e949-4a23-49f7-bf27-ca140852cf21)" width="52" height="24"></rect>
-          </svg>
-          <span class="relative text-6xl">latest products</span>
-        </span class=" font-semibold">
-        affordable for everyone
-      </h2>
-    </div>
-
-
-   <div class="grid max-w-screen-md gap-10 md:grid-cols-2 sm:mx-auto">
-   <div>
-        <div class="p-4  bg-gray-50 rounded-xl shadow-lg shadow-black">
-          <div class="mb-4 text-center flex flex-col">
-
-      <?php  
-         $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
-         if(mysqli_num_rows($select_products) > 0){
-            while($fetch_products = mysqli_fetch_assoc($select_products)){
-      ?>
-      <div class="mb-4 text-center ">
-     <form action="" method="post" class="flex flex-col items-center">
-      <img class="relative  items-center text-white rounded-xl w-96 h-auto" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
-     
-      <div class="text-4xl font-medium tracking-wide text-white"><?php echo $fetch_products['name']; ?></div>
-            
-      
-      <div class="flex text-2xl font-semibold text-black lg:text-3xl absolute bg-orange-400 ml-96  rounded-xl p-1"">$<?php echo $fetch_products['price']; ?>/-</div>
-
-     
-      <!-- <input type="number" min="1" name="product_quantity" value="1" class="qty"> -->
-      <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-      <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-      <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-      <input  type="submit"  value="add to cart" name="add_to_cart"
-            class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-          >
-          </div>
-     </form>
-     
-      </div>
-      <?php
-         }
-      }else{
-         echo '<p class="empty">no products added yet!</p>';
-      }
-      ?>
-          </div>
-        </div>  
-   </div>
-
-   <div class="load-more" style="margin-top: 2rem; text-align:center">
-  
-         
-      <a href="shop.php" class="option-btn">load more</a>
-   </div>
 
 
  
-   
- </div>
+
+
+    <section class="products">
+
+<h1 class="title">latest products</h1>
+
+<div class="box-container">
+
+   <?php  
+      $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+      if(mysqli_num_rows($select_products) > 0){
+         while($fetch_products = mysqli_fetch_assoc($select_products)){
+   ?>
+  <form action="" method="post" class="box">
+   <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+   <div class="name"><?php echo $fetch_products['name']; ?></div>
+   <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+   <input type="number" min="1" name="product_quantity" value="1" class="qty">
+   <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+   <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+   <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+   <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+  </form>
+   <?php
+      }
+   }else{
+      echo '<p class="empty">no products added yet!</p>';
+   }
+   ?>
+</div>
+
 </section>
 
-<!-- tailwind landing 1 -->
-<!--
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
--->
-
-<div class="bg-white">
-  <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
-
-    <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-      <div class="group relative">
      
-        <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-        <?php
-         $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
-         if(mysqli_num_rows($select_products) > 0){
-            while($fetch_products = mysqli_fetch_assoc($select_products)){
-      ?>
 
-        <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover p-2 lg:w-full lg:h-full">
-        
-        </div>
-        <div class="mt-4 flex justify-between">
-          <div>
-            <h3 class="text-sm text-gray-900">
-              <a href="#">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                <?php echo $fetch_products['name']; ?>
-              </a>
-            </h3>
-            <p class="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p class="text-sm font-medium text-gray-900">$<?php echo $fetch_products['price']; ?></p>
-        </div>
-        
-      </div>
-
-      <!-- More products... -->
-      
-    </div>
-  </div>
-</div>
-<?php
-         }
-      }else{
-         echo '<p class="empty">no products added yet!</p>';
-      }
-      ?>
-
-
-<!-- end -->
 
 
 <?php include 'footer.php'; ?>
