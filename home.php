910803820f648e73c2b7b8731777caefc6633234
haamid10 <apdixamiid919@gmail.com>
2022-08-14 12:55:00 +0300

    product in home2

--- home.php
+++ home.php
@@ -98,6 +98,10 @@ if(isset($_POST['add_to_cart'])){
 
 </section> -->
 
+
+
+
+
 </div>
 <section class="relative w-full h-full flex flex-row">
 <div class="absolute hidden w-full bg-gray-50 lg:block h-96"></div>
@@ -175,6 +179,65 @@ if(isset($_POST['add_to_cart'])){
 </section>
 
 <!-- tailwind landing 1 -->
+<!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
+<div class="bg-white">
+  <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+
+    <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      <div class="group relative">
+     
+        <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+        <?php
+         $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+         if(mysqli_num_rows($select_products) > 0){
+            while($fetch_products = mysqli_fetch_assoc($select_products)){
+      ?>
+
+        <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="Front of men&#039;s Basic Tee in black." class="w-fit h-full object-center object-cover lg:w-fit lg:h-full">
+        
+        </div>
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Basic Tee
+              </a>
+            </h3>
+            <p class="mt-1 text-sm text-gray-500">Black</p>
+          </div>
+          <p class="text-sm font-medium text-gray-900">$35</p>
+        </div>
+      </div>
+
+      <!-- More products... -->
+      
+    </div>
+  </div>
+</div>
+<?php
+         }
+      }else{
+         echo '<p class="empty">no products added yet!</p>';
+      }
+      ?>
+
 
 <!-- end -->
 
