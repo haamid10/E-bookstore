96bc9246b6d236c32affc91a8a8b6a92f0396afb
haamid10 <apdixamiid919@gmail.com>
2022-08-20 21:42:54 +0300

    dashboard

--- admin_sales.php
+++ admin_sales.php
@@ -75,7 +75,7 @@ if(isset($_POST['search'])){
    </form>
 </section>
 
-<section  >
+
 
    <div >
    <?php
@@ -86,40 +86,30 @@ if(isset($_POST['search'])){
          while($fetch_sales = mysqli_fetch_assoc($select_sales)){
    ?>
    <form action="" method="post" >
-     <table>
-     <colgroup>
-                    <col >
-                    <col >
-                    <col >
-                    <col >
-                    <col >
-                    <col >
-                    <col >
-                    <col >
-                    <col >
-                </colgroup>
-                <thead>
-                    <tr>
-                       
+   <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Summury Report </h2>
+                <table class="table table-bordered   " >
+                    <thead>
+                        <tr>
                         <th>user Id</th>       
                         <th>name</th>          
                         <th>phone</th>         
-                        <th>total price</th>          
-                        <th>city</th> 
-                        <!-- <th>country</th> -->
-                        <th>street</th>
-                         <th>quantity</th>        
-                        <th>payment status</th>
+                        <th>city</th>          
+                        <th>country</th>       
+                        <th>street</th>        
+                        <th>email</th>         
+                        <th>quantity</th>       
+                        <th>price</th>           
                         <th>payment method</th>
-                        <th>email</th>
-                        <th>placed on</th>   
-                        <th>country</th>  
-                        
-                        
-                    </tr>
-                </thead>
+                        <th>placed on</th>     
+                        <th>payment status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                 
-                <tbody>
+                
       <input type="hidden"  class="qty" name="total_product" min="1" value="1">
       <input type="hidden" name="id" value="<?php echo $fetch_sales['id']; ?>">
      <td> <input type="text" name="user_id" value="<?php echo $fetch_sales['user_id']; ?>"></td>
@@ -157,35 +147,69 @@ if(isset($_POST['search'])){
    ?>
    
    </div>
-   <tr>
-                        <td class="text-center" colspan="20">___________________________________________________________________________________________</td>
-                    </tr>
-                    <tr>
-                        <td class="text-center" colspan="20">___________________________________________________________________________________________</td>
-                    </tr>
-
-    </section>
+   
+   
 <!-- ende of search -->
 
+<?php 
+include 'config.php';
+
+?>
 
 
+<!DOCTYPE html>
+<html lang="en">
 
-       
-   <table class="table table-bordered">
-                <colgroup>
-                    <col width="60">
-                    <col width="60">
-                    <col width="60">
-                    <col width="60">
-                    <col width="60">
-                    <col width="60">
-                    <col width="60">
-                    <col width="60">
-                    <col width="60">
-                </colgroup>
-                <thead>
-                    <tr>
-                       
+
+<head>
+
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sales</title>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="print.css" media="print">
+    <!-- Font Awesome -->
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!-- CSS -->
+    <script src="https://kit.fontawesome.com/d8cfbe84b9.js" crossorigin="anonymous"></script>
+
+    <?php
+    
+    include 'config.php';
+    
+    ?>
+    <style>
+        .assets.styles.admin {
+            display: none
+        }
+
+        #print-btn {
+            border: white;
+
+        }
+
+        button {
+            border: none;
+        }
+    </style>
+
+</head>
+
+<body>
+    <?php include 'config.php'; ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Summury Report </h2>
+                <table class="table table-bordered   " >
+                    <thead>
+                        <tr>
                         <th>user Id</th>       
                         <th>name</th>          
                         <th>phone</th>         
@@ -198,76 +222,63 @@ if(isset($_POST['search'])){
                         <th>payment method</th>
                         <th>placed on</th>     
                         <th>payment status</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                
-      $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
-      if(mysqli_num_rows($select_orders) > 0){
-         while($fetch_orders = mysqli_fetch_assoc($select_orders)){
-      ?>
-                   
-                        <td> <span><?php echo $fetch_orders['user_id']; ?></td>
-                        <td>
-                            <p class="m-0"><?php echo $fetch_orders['name'] ?></p>
-                        </td>
-                        <td>
-                            <p class="m-0"><?php echo $fetch_orders['number'] ?></p>
-                        </td>
-                        <td>
-                            <p class="m-0"><?php echo $fetch_orders['city']?></p>
-                        </td>
-                        <td>
-                            <p class="m-0"><?php echo $fetch_orders['country']?></p>
-                        </td>
-                        <td>
-                            <p class="m-0"><?php echo $fetch_orders['street']?></p>
-                        </td>
-                        <td>
-                            <p class="m-0"><?php echo $fetch_orders['email'] ?></p> </td>
-                        <td class="text-center"><?php echo $fetch_orders['total_products']?></td>
-                       
-                        <td class="text-right">$<?php echo $fetch_orders['total_price'] ?></td>
-                        <td class="text-center"><?php echo $fetch_orders['method']?></td>
-                        <td class="text-center"><?php echo $fetch_orders['placed_on']?></td>
-                        <td class="text-center"><?php echo $fetch_orders['payment_status']?></td>
-                        <td>   <a href="admin_sales.php?delete=<?php echo $fetch_orders['id']; ?>" onclick="return confirm('delete this order?');" class="delete-btn">delete</a> </td>
-                    </tr>
-                   
-                    <?php if($qry->num_rows <= 0): ?>
-                    <tr>
-                        <td class="text-center" colspan="20">___________________________________________________________________________________________</td>
-                    </tr>
-                    <?php endif; ?>
-      
+                        </tr>
+                    </thead>
+                    <tbody>
+                      
+                        <?php
+                        // $sn = 1;
+                        $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
+                        if(mysqli_num_rows($select_orders) > 0){
+                           while($fetch_orders = mysqli_fetch_assoc($select_orders)){
+                       ?>
+                            <tr>
+                                <td><?php echo $fetch_orders['user_id'];  ?></td>
+                                <td><?php echo $fetch_orders['name'];  ?></td>
+                                <td><?php echo $fetch_orders['number'];  ?></td>
+                                <td><?php echo $fetch_orders['city'];  ?></td>
+                                <td><?php echo $fetch_orders['country'];  ?></td>
+                                <td><?php echo $fetch_orders['street'];  ?></td>
+                                <td><?php echo $fetch_orders['email'];  ?></td>
+                                <td><?php echo $fetch_orders['total_products'];  ?></td>
+                                <td><?php echo $fetch_orders['total_price'];  ?></td>
+                                
+                                
+                                
+                                <td><?php echo $fetch_orders['payment_status'];  ?></td>
+                                <td><?php echo $fetch_orders['placed_on'];  ?></td>
+                                <td><?php echo $fetch_orders['method'];  ?></td>
+                               
+                            </tr>
+                        <?php
+                            $sn++;
+                        }
+                      }
+                    
+                        ?>
+                    </tbody>
 
-                </tbody>
-                <?php
-         }
-      }else{
-         echo '<p class="empty">no sales placed yet!</p>';
-      }
-      
-      ?>
-            
-            
-            
-            
-            
-            
-            
-            
-            <a href="sales_print.php" >print111</a>            
-         <script type="text/javascript">
+                </table>
+                <div class="text-center">
+                <button  class="btn btn-primary" id="print-btn"><a href="sales_print.php"> print now </a></button>
+                </div>
 
-       function myFun(){
-          window.print();
-       }
-            </script>
-            </table>
 
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+
+
+    <script src="../assets/scripts/admin_bus.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+
+</html>
 
 
 
