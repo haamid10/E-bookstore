5afdde28b4bb71bbd3ae2547be1a70bb988e2aa9
haamid10 <apdixamiid919@gmail.com>
2022-08-20 21:00:39 +0300

    dashboard

--- sales_print.php
+++ sales_print.php
@@ -1,6 +1,6 @@
 <?php 
 include 'config.php';
-// require '../assets/partials/_admin-check.php';
+
 ?>
 
 
@@ -28,9 +28,7 @@ include 'config.php';
     <?php
     
     include 'config.php';
-    // require '../assets/styles/admin.php';
-    // require '../assets/styles/admin-options.php';
-    // $page = "bus";
+    
     ?>
     <style>
         .assets.styles.admin {
@@ -50,7 +48,6 @@ include 'config.php';
 </head>
 
 <body>
-    <!-- Requiring the admin header files -->
     <?php include 'config.php'; ?>
 
     <div class="container">
@@ -125,9 +122,7 @@ include 'config.php';
 
 
 
-    <!-- External JS -->
     <script src="../assets/scripts/admin_bus.js"></script>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
