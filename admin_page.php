803386964572184a2c8c3a939b012944684e0932
haamid10 <apdixamiid919@gmail.com>
2022-08-22 23:10:28 +0300

    main page1

--- admin_page.php
+++ admin_page.php
@@ -26,6 +26,21 @@ if(!isset($admin_id)){
    <!-- custom admin css file link  -->
    <link rel="stylesheet" href="css/admin_style.css">
 
+   <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
 </head>
 <body>
    
@@ -57,13 +72,7 @@ if(!isset($admin_id)){
                 
             
 
-                <div class="card-body">
-                  <h5 class="card-title">Total Pending</h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-cart"></i>
-                    </div>
+                
 
 
 
@@ -77,14 +86,25 @@ if(!isset($admin_id)){
                };
             };
          ?>
-         
-         <h3 class="ps-3" >$<?php echo $total_pendings; ?>/-</h3>
-         <p class="text-success small pt-1 fw-bold">total pendings</p>
-      
+         <div class="card-body">
+                  <h5 class="card-title">Total Pending</h5>
+
+                  <div class="d-flex align-items-center">
+                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                      <i class="bi bi-cart"></i>
+                    </div>
+         <div class="ps-3">
+         <h6>$<?php echo $total_pendings; ?></h6>
+         </div>
+
+             
+       
       </div>
                   </div>
                 </div>
               </div>
+          </div>
+        </div>
 
       <div class="box">
          <?php
