b59140cf605b0e2249139c8dd4af61f47fd1a917
haamid10 <apdixamiid919@gmail.com>
2022-08-27 20:15:30 +0300

    admin users9

--- admin_users.php
+++ admin_users.php
@@ -199,10 +199,16 @@ if(isset($_POST['search'])){
 </section>
 <!--  end of users list-->
 
-<section class="section">
 
 
 
+
+
+
+<section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
  
 
 
@@ -220,8 +226,60 @@ if(isset($message)){
 }
 ?>
    
-<div class="user">
+<div class="card mb-3">
 
+<div class="card-body">
+
+  <div class="pt-4 pb-2">
+    <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
+    <p class="text-center small">Enter your personal details to create account</p>
+  </div>
+
+  <form class="row g-3 needs-validation" method="post">
+    <div class="col-12">
+      <label for="yourName" class="form-label">Your Name</label>
+      <input type="text" name="name" placeholder="enter your name" required  class="form-control">
+      <div class="invalid-feedback">Please, enter your name!</div>
+    </div>
+
+    <div class="col-12">
+      <label for="yourEmail" class="form-label">Your Email</label>
+      <input type="email" name="email" placeholder="enter your email" required  class="form-control">
+      <div class="invalid-feedback">Please enter a valid Email adddress!</div>
+    </div>
+
+   
+
+    <div class="col-12">
+      <label for="yourPassword" class="form-label">Password</label>
+      <input type="password" name="password" class="form-control" id="yourPassword" required>
+      <div class="invalid-feedback">Please enter your password!</div>
+    </div>
+    <div class="col-12">
+      <label for="yourPassword" class="form-label">Re-enter your password </label>
+      <input type="password" name="cpassword" placeholder="confirm your password" required  class="form-control"> 
+      <div class="invalid-feedback">Please enter your password!</div>
+    </div>
+    <div class="col-12">
+    <select name="user_type"  class="form-control">
+         <option value="user">user</option>
+         <option value="admin">admin</option>
+      </select>
+    </div>
+
+    <div class="col-12">
+      <div class="form-check">
+        <input class="form-check-input" name="terms" type="checkbox" value="" id="acceptTerms" required>
+        <label class="form-check-label" for="acceptTerms">I agree and accept the terms and conditions</label>
+        <div class="invalid-feedback">You must agree before submitting.</div>
+      </div>
+    </div>
+    <div class="col-12">
+      <input class="btn btn-primary w-100" type="submit" name="submit" value="register now" >
+
+      <!-- <button class="btn btn-primary w-100" type="submit">Create Account</button> -->
+    </div>
+<!-- 
    <form class="box-container" action="" method="post">
       <h3 class="title">Add user</h3>
       <input type="text" name="name" placeholder="enter your name" required class="box">
@@ -236,7 +294,7 @@ if(isset($message)){
 
     
       
-   </form>
+   </form> -->
 
   
    </div>
