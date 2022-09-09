6bb8990a6a79c66401786542bfee93d3d5fee9e9
haamid10 <apdixamiid919@gmail.com>
2022-09-10 00:23:27 +0300

    orders in home 7

--- orders.php
+++ orders.php
@@ -19,6 +19,13 @@ if(!isset($user_id)){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>orders</title>
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/admin_style.css">
+
    <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
@@ -30,12 +37,10 @@ if(!isset($user_id)){
   <link href="../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="../assets/vendor/simple-datatables/style.css" rel="stylesheet">
-  <script src="https://cdn.tailwindcss.com"></script>
 
   <!-- Template Main CSS File -->
-  <!-- <link href="../assets/css/style.css" rel="stylesheet"> -->
-  <!-- <link rel="stylesheet" href="../css/admin_style.css"> -->
-
+  <link href="../assets/css/style.css" rel="stylesheet">
+  <script src="https://cdn.tailwindcss.com"></script>
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
@@ -129,38 +134,24 @@ if(!isset($user_id)){
 </div>
 
 
-<section class="placed-orders">
+  
+<main class="main" id="main">
 
-   <h1 class="title">placed orders</h1>
+<div class="pagetitle">
+      <h1>Manage orders</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="../admin_page.php">Home   </a></li>
+          <li class="breadcrumb-item active"><a href="../admin_orders.php">Orders  </a></li>
+        </ol>
+      </nav>
+    </div>
 
-   <div class="box-container">
-
-      <?php
-         $order_query = mysqli_query($conn, "SELECT * FROM `orders` WHERE user_id = '$user_id'") or die('query failed');
-         if(mysqli_num_rows($order_query) > 0){
-            while($fetch_orders = mysqli_fetch_assoc($order_query)){
-      ?>
-      <div class="box">
-         <p> placed on : <span><?php echo $fetch_orders['placed_on']; ?></span> </p>
-         <p> name : <span><?php echo $fetch_orders['name']; ?></span> </p>
-         <p> number : <span><?php echo $fetch_orders['number']; ?></span> </p>
-         <p> email : <span><?php echo $fetch_orders['email']; ?></span> </p>
-         <p> address : <span><?php echo $fetch_orders['address']; ?></span> </p>
-         <p> payment method : <span><?php echo $fetch_orders['method']; ?></span> </p>
-         <p> your orders : <span><?php echo $fetch_orders['total_products']; ?></span> </p>
-         <p> total price : <span>$<?php echo $fetch_orders['total_price']; ?></span> </p>
-         <p> payment status : <span style="color:<?php if($fetch_orders['payment_status'] == 'pending'){ echo 'red'; }else{ echo 'green'; } ?>;"><?php echo $fetch_orders['payment_status']; ?></span> </p>
-         </div>
-      <?php
-       }
-      }else{
-         echo '<p class="empty">no orders placed yet!</p>';
-      }
-      ?>
-   </div>
-
-
-</section>
+   <section class="bg-white ">
+ 
+ <!-- <div class="col-lg-32"> -->
+    
+<h1 class=" text-center font-bold text-blue-700 ">user orders</h1>
 
 <section class="section">
 
@@ -186,6 +177,82 @@ if(!isset($user_id)){
                         <th scope="col">date</th>     
                         <th scope="col"> status</th>
                         <th scope="col"> update</th>
+                       
+
+                  </tr>
+                </thead>
+                <tbody>
+                <?php
+                        // $sn = 1;
+                        $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
+                        if(mysqli_num_rows($select_orders) > 0){
+                           while($fetch_orders = mysqli_fetch_assoc($select_orders)){
+                       ?>
+                  <tr>
+                  <td scope="row"><?php echo $fetch_orders['user_id'];  ?></td>
+                                <td><?php echo $fetch_orders['name'];  ?></td>
+                                <td><?php echo $fetch_orders['number'];  ?></td>
+                                <td><?php echo $fetch_orders['city'];  ?></td>
+                           
+                                <td><?php echo $fetch_orders['email'];  ?></td>
+                                <td><?php echo $fetch_orders['total_products'];  ?></td>
+                                <td><?php echo $fetch_orders['total_price'];  ?></td>
+
+                                
+                                <td><?php echo $fetch_orders['payment_status'];  ?></td>
+                                <td><?php echo $fetch_orders['placed_on'];  ?></td>
+                                <td><?php echo $fetch_orders['method'];  ?></td>
+                               
+       
+          
+            
+         
+                                
+                  </tr>
+                  <?php
+                            $sn++;
+                        }
+                      }
+                    
+                        ?>
+                </tbody>
+              </table>
+              <!-- End small tables -->
+
+            </div>
+          </div>
+  </div>
+</section>
+
+</section>
+
+
+</main>
+
+
+<section class="section">
+
+  <div class="col-lg-16">
+   <div class="card">
+            <div class="card-body">
+              <!-- <h5 class="card-title">Order  tables</h5> -->
+             
+              <!-- Small tables -->
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                   
+                        <th scope="col"> Id</th>       
+                        <th scope="col">name</th>          
+                        <th scope="col">phone</th>         
+                        <th scope="col">city</th>          
+                           
+                        <th scope="col">email</th>         
+                        <th scope="col">quantity</th>       
+                        <th scope="col">price</th>           
+                        <th scope="col"> method</th>
+                        <th scope="col">date</th>     
+                        <th scope="col"> status</th>
                        
 
                   </tr>
