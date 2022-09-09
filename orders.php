3ee2f752ea5b2062b4d6c2ab1c0070ad81c60266
haamid10 <apdixamiid919@gmail.com>
2022-09-09 23:52:24 +0300

    orders in home 4

--- orders.php
+++ orders.php
@@ -158,6 +158,87 @@ if(!isset($user_id)){
       }
       ?>
    </div>
+   <section class="section">
+
+  <div class="col-lg-16">
+   <div class="card">
+            <div class="card-body">
+              <!-- <h5 class="card-title">Order  tables</h5> -->
+             
+              <!-- Small tables -->
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                   
+                        <th scope="col"> Id</th>       
+                        <th scope="col">name</th>          
+                        <th scope="col">phone</th>         
+                        <th scope="col">city</th>          
+                           
+                        <th scope="col">email</th>         
+                        <th scope="col">quantity</th>       
+                        <th scope="col">price</th>           
+                        <th scope="col"> method</th>
+                        <th scope="col">date</th>     
+                        <th scope="col"> status</th>
+                        <th scope="col"> update</th>
+                       
+
+                  </tr>
+                </thead>
+                <tbody>
+                <?php
+                        // $sn = 1;
+                        $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
+                        if(mysqli_num_rows($select_orders) > 0){
+                           while($fetch_orders = mysqli_fetch_assoc($select_orders)){
+                       ?>
+                  <tr>
+                  <td scope="row"><?php echo $fetch_orders['user_id'];  ?></td>
+                                <td><?php echo $fetch_orders['name'];  ?></td>
+                                <td><?php echo $fetch_orders['number'];  ?></td>
+                                <td><?php echo $fetch_orders['city'];  ?></td>
+                           
+                                <td><?php echo $fetch_orders['email'];  ?></td>
+                                <td><?php echo $fetch_orders['total_products'];  ?></td>
+                                <td><?php echo $fetch_orders['total_price'];  ?></td>
+
+                                
+                                <td><?php echo $fetch_orders['payment_status'];  ?></td>
+                                <td><?php echo $fetch_orders['placed_on'];  ?></td>
+                                <td><?php echo $fetch_orders['method'];  ?></td>
+                               
+            <td> <form action="" method="post">
+               <input type="hidden" name="order_id" value="<?php echo $fetch_orders['id']; ?>"></td>
+            <td><select name="update_payment">
+               <option value="" selected disabled><?php echo $fetch_orders['payment_status']; ?></option>
+               <option value="pending">pending</option>
+               <option value="completed">completed</option>
+            </select></td>
+         
+           <td> <input type="submit" value="update" name="update_order" class="btn btn-secondary"></td> 
+           <td>   <a href="admin_orders.php?delete=<?php echo $fetch_orders['id']; ?>" onclick="return confirm('delete this order?');" class="btn btn-primary">delete</a>
+           </form>
+         </td>
+          
+            
+         
+                                
+                  </tr>
+                  <?php
+                            $sn++;
+                        }
+                      }
+                    
+                        ?>
+                </tbody>
+              </table>
+              <!-- End small tables -->
+
+            </div>
+          </div>
+  </div>
+</section>
 
 </section>
 
