219de61fa071456bc15d9293509402aa87cd59d8
haamid10 <apdixamiid919@gmail.com>
2022-09-09 19:37:52 +0300

    orders in home 2

--- header.php
+++ header.php
@@ -32,7 +32,7 @@ if(isset($message)){
          <nav class="navbar">
             <a href="home.php">home</a>
             <a href="about.php">about</a>
-            <a href="shop.php">shop</a>
+            <a href="shop.php">Products</a>
             <a href="contact.php">contact</a>
             <a href="orders.php">orders</a>
          </nav>
