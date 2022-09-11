4b7d850432ca6f0b3e6f68e4baa74cdf09d62272
haamid10 <apdixamiid919@gmail.com>
2022-09-11 09:12:36 +0300

    xaabxaa  6

--- orders.php
+++ orders.php
@@ -45,7 +45,7 @@ if(!isset($user_id)){
 
 </head>
    <!-- font awesome cdn link  -->
-   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"> -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <!-- custom css file link  -->
 <link rel="stylesheet" href="css/style.css">
