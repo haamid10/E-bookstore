b4df8cc30b5d588b44edb7b36fb28b324c4b853e
haamid10 <apdixamiid919@gmail.com>
2022-09-11 09:37:51 +0300

    xaabxaa  9

--- about.php
+++ about.php
@@ -31,11 +31,40 @@ if(!isset($user_id)){
 
 </head>
 <body>
+
+
+<section class="about">
+
+<div class="flex">
+
    
+
+  
+
+   <main class=" mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+     <div class="sm:text-center lg:text-left">
+       <h1 class="text-2xl text-right font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+         <span class="block xl:inline capitalize">Read and visit us to Grow</span>
+         <span class="block text-indigo-600 xl:inline">your mind</span>
+       </h1>
+       <p class="mt-3 text-center  text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-44">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+       
+     </div>
+     
+   </main>
+   <div class="image">
+      <img class="rounded-tr-l  rounded-t-2xl" src="./uploaded_img/books.jpg" alt="">
+   </div>
+</div>
+
+</section> 
+
+
 <section>
 
-<div class="bg-white py-12">
-  <div class="mx-auto  lg:px-8">
+
+ <div class="bg-white py-12">
+  <div class="mx-32 mt-12 mb  lg:px-8">
     <div class="lg:text-center">
       <h2 class="text-6xl font-semibold text-indigo-600">Reading</h2>
       <p class="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">Is a better way to invest your time</p>
@@ -69,7 +98,7 @@ if(!isset($user_id)){
           </dt>
           <dd class="mt-2 ml-16 text-base text-gray-500">Brain scans showed that throughout the reading period and for days afterward, brain connectivity increased, especially in the somatosensory cortex, the part of the brain that responds to physical sensations like movement and pain.
 
-</dd>
+   </dd>
         </div>
 
         <div class="relative">
@@ -82,7 +111,7 @@ if(!isset($user_id)){
             </div>
             <p class="ml-16 text-xl font-medium leading-6 text-gray-900">Keeping your brain in shape</p>
           </dt>
-          <dd class="mt-2 ml-16 text-base text-gray-500">Keeping your brain in shape.</dd>
+          <dd class="mt-2 ml-16 text-base text-gray-500">Mental fitness means keeping your brain and emotional health in tip-top shape. It doesn’t mean training for “brain Olympics” or acing an IQ test. It refers to a series of exercises that help you.</dd>
         </div>
 
         <div class="relative">
@@ -105,77 +134,7 @@ if(!isset($user_id)){
 
 
 
-<div class="relative overflow-hidden bg-white">
-  <div class="mx-auto max-w-7xl">
-    <div class="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
-      <svg class="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-        <polygon points="50,0 100,0 50,100 0,100" />
-      </svg>
 
-      <div>
-        <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
-          <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-            <div class="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
-              <div class="flex w-full items-center justify-between md:w-auto">
-                <!-- <a href="#">
-                  <span class="sr-only">Your Company</span>
-                  <img alt="Your Company" class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600">
-                </a> -->
-                <div class="-mr-2 flex items-center md:hidden">
-                  <button type="button" class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <!-- Heroicon name: outline/bars-3 -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-           
-          </nav>
-        </div>
-
-        <!--
-          Mobile menu, show/hide based on menu open state.
-
-          Entering: "duration-150 ease-out"
-            From: "opacity-0 scale-95"
-            To: "opacity-100 scale-100"
-          Leaving: "duration-100 ease-in"
-            From: "opacity-100 scale-100"
-            To: "opacity-0 scale-95"
-        -->
-        <div class="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden">
-          <div class="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
-            <div class="flex items-center justify-between px-5 pt-4">
-              <div>
-                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
-              </div>
-             
-            </div>
-            
-            <a href="#" class="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-indigo-600 hover:bg-gray-100">Log in</a>
-          </div>
-        </div>
-      </div>
-
-      <main class=" mt-10 max-w-7xl px-4  lg:mt-20 lg:px-8 xl:mt-28">
-        <div class="sm:text-center lg:text-left">
-          <h1 class="text-4xl text-left mr-12 font-bold tracking-wider text-gray-900 ">
-            <span class="block xl:inline capitalize">Read and visit us to Grow</span>
-            <span class=" text-indigo-600 xl:inline">your mind</span>
-          </h1>
-          <p class="mt-3 text-center  text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-44">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-          
-        </div>
-      </main>
-    </div>
-  </div>
-  <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full" src="./uploaded_img/books.jpg" alt="">
-  </div>
-</div>
 
 
 <section class="about">
@@ -183,7 +142,7 @@ if(!isset($user_id)){
    <div class="flex">
 
       <div class="image">
-         <img src="images/about-img.jpg" alt="">
+         <img class="rounded-tr-l  rounded-t-2xl" src="./uploaded_img/books.jpg" alt="">
       </div>
 
      
