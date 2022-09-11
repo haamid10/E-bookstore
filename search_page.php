d3a83e27e605f0ac54347a79a9720e1fa4e919f3
haamid10 <apdixamiid919@gmail.com>
2022-09-11 09:12:58 +0300

    xaabxaa  7

--- search_page.php
+++ search_page.php
@@ -49,10 +49,7 @@ if(isset($_POST['add_to_cart'])){
    
 <?php include 'header.php'; ?>
 
-<div class="heading">
-   <h3>search page</h3>
-   <p> <a href="home.php">home</a> / search </p>
-</div>
+
 
 <section class="search-form">
    <form action="" method="post">
